54426b44ed3b6de8b3f357a80afa472b6fea5720
Agrima <er.agrima.malhotra@gmail.com>
2023-01-30 10:52:56 +0530

    feat: add get and post request

--- expressServer.js
+++ expressServer.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 const PORT = 6000;
-
+var taskId = 3;
 //Inital ToDo DB
 var data = {
   userId: 1,
@@ -24,6 +24,59 @@ var data = {
     }
   ]
 };
+
+const generateTask = (task) => {
+  taskId++;
+  return {
+    id: taskId,
+    task: task,
+    isComplete: false
+  };
+}
+const getIdEntry = (id, dataTask) => {
+  let idx;
+  dataTask.forEach((obj, index) => {
+    if (obj.id == id)
+      idx = index;
+  });
+  return idx;
+}
+server.route('/tasks')
+  .get((request, response) => {
+    console.log('GET Request');
+    response.send(data);
+  })
+  .post((request, response) => {
+    console.log('POST Request');
+    data.tasks.push(generateTask(request.body.tasks));
+    response.send(data);
+  });
+server.route('/task/:id')
+  .get((request, response) => {
+    console.log('GET Request');
+    const id = Number(request.params.id);
+    response.send(data.tasks[getIdEntry(id, data.tasks)]);
+  })
+  .put((request, response) => {
+    console.log('PUT Request');
+    const id = Number(request.params.id);
+    data.tasks[getIdEntry(id, data.tasks)] = request.body;
+    response.send(data);
+  })
+  .patch((request, response) => {
+    console.log('PATCH Request');
+    const id = Number(request.params.id);
+    const keys = Object.keys(request.body);
+    keys.forEach(key => data.tasks[getIdEntry(id, data.tasks)][key] = request.body[key]);
+    response.send(data);
+  })
+  .delete((request, response) => {
+    console.log('DELETE Request');
+    const id = Number(request.params.id);
+    data.tasks.splice(getIdEntry(id, data.tasks), 1);
+    response.send(data);
+  });
+
 
 server.listen(PORT, () => {
   console.log(`ToDo App at Port ${PORT}`);
